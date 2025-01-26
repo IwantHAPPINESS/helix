c8fe2950d4a438ca2451f8a0f86303215516dbe3
IwantHAPPINESS <magomadovimran142@gmail.com>
2025-01-26 07:35:32 +0300

    add js,pyhton,sql lsp

---
+++ install-lsp.bat
@@ -0,0 +1,15 @@
+winget install clangd
+rustup component add rust-analyzer
+cargo install taplo-cli --locked --features lsp
+pip install ruff
+npm install pyright -g
+npm i -g vscode-langservers-extracted
+npm install -g typescript typescript-language-server
+npm install -g @microsoft/compose-language-service
+npm install -g dockerfile-language-server-nodejs
+go install golang.org/x/tools/gopls@latest
+go install golang.org/x/tools/cmd/goimports@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install golang.org/x/tools/cmd/goimports@latest
+go install github.com/nametake/golangci-lint-langserver@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
