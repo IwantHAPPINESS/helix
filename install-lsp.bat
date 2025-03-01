bcc81b9ddccc6bbe053ca528679b9cd35794f859
IwantHappiness <magomadovimran142@gmail.com>
2025-03-01 07:04:23 +0000

    add zig lsp

--- install-lsp.bat
+++ install-lsp.bat
@@ -7,9 +7,9 @@ npm i -g vscode-langservers-extracted
 npm install -g typescript typescript-language-server
 npm install -g @microsoft/compose-language-service
 npm install -g dockerfile-language-server-nodejs
-go install golang.org/x/tools/gopls@latest
-go install golang.org/x/tools/cmd/goimports@latest
-go install github.com/go-delve/delve/cmd/dlv@latest
-go install golang.org/x/tools/cmd/goimports@latest
-go install github.com/nametake/golangci-lint-langserver@latest
+go install golang.org/x/tools/gopls@latest \
+go install golang.org/x/tools/cmd/goimports@latest \
+go install github.com/go-delve/delve/cmd/dlv@latest \
+go install golang.org/x/tools/cmd/goimports@latest \
+go install github.com/nametake/golangci-lint-langserver@latest \
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
